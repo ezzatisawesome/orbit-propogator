24ecf22e1e7f010aa450ec4eb05f12ae79b02eba
ezzatisawesome <ezzatsuhaime@gmail.com>
2023-11-01 12:16:33 -0700

    Updates

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from src.plot import plot_groundtracks, plot_eci
 from src.utils import in_eclipse
 
 # Simulation parameters
-sim_duration = 60.0 * 60 * 24 * 60 # seconds
+sim_duration = 60.0 * 60 * 24 * 365 # seconds
 dt = 10
 
 # Epoch (Vernal Equinox 2024)
@@ -21,18 +21,20 @@ second = 0
 t0 = datetime(year, month, day, hour, minute, second).timestamp()
 
 # Satellite orbit
-coesSat = [7641.80, 0.0, 98.94, 0, 0, 0]  # sma, ecc, inc, ta, aop, raan, t
+coesSat = [7641.80, 0.00000001, 100.73, 0, 0, 90]  # sma, ecc, inc, ta, aop, raan
 orbit = Orbit(coesSat)
 satellite = Satellite(1400, orbit)  # ! Need to fix satellite mass
 propagateSat = Propagate(satellite, sim_duration, dt)
 statesSat, statesGeocSat = propagateSat.propagate(t0, options={'j2': True})
 
 # Sun orbit
-coesSun = [149.598e6, 0.0167, 23.44, 0, 0, 0]
+coesSun = [149.598e6, 0.0167, 23.4406, 0, 282.7685, 0]
 orbitSun = Orbit(coesSun)
 sun = Satellite(1.989e30, orbitSun)
 propagateSun = Propagate(sun, sim_duration, dt)
-statesSun, statesGeocSun = propagateSun.propagate(t0, options={'j2': False})
+statesSun, statesGeocSun = propagateSun.propagate(t0, options={'j2': True})
+print(statesSun)
+print(statesSat)
 
 # Check for eclipses
 stateEclipse, sunDot, perpNorm = in_eclipse(statesSat, statesSun)
@@ -48,5 +50,5 @@ for (i, state) in enumerate(stateEclipse):
 file.close()
 
 # plot_groundtracks(statesGeocSat)
-# plot_eci([statesSat], {'show': True, 'cb_axes_color': 'k'})
+# plot_eci([statesSat, statesSun], {'show': True, 'cb_axes_color': 'k'})
 # plt.show()
--- src/orbit.py
+++ src/orbit.py
@@ -30,3 +30,41 @@ class Orbit:
         v_rot = v_w @ R.as_matrix()
 
         return np.concatenate((r_rot, v_rot))
+    
+    @staticmethod
+    def State2Coes(state: np.ndarray[float]) -> Tuple[float, float, float, float, float, float]:
+        r_vec = state[:3]
+        v_vec = state[3:]
+
+        # Position and velocity magnitudes
+        r = np.linalg.norm(r_vec)
+        v = np.linalg.norm(v_vec)
+        v_r = np.dot(r_vec / r, v_vec)
+        v_p = np.sqrt(v ** 2 - v_r ** 2)
+
+        # Orbital angular momentum
+        h_vec = np.cross(r_vec, v_vec)
+        h = np.linalg.norm(h_vec)
+        sma = h
+
+        # Inclination
+        inc = np.arccos(h_vec[2] / h)
+
+        # RAAN
+        K = np.array((0, 0, 1))
+        N_vec = np.cross(K, h_vec)
+        N = np.linalg.norm(N_vec)
+        raan = 2 * np.pi - np.arccos(N_vec[0] / N)
+
+        # Eccentricity
+        e_vec = np.cross(v_vec, h_vec) / earth_mu - r_vec / r
+        ecc = np.linalg.norm(e_vec)
+
+        # AOP
+        aop = 2 * np.pi - np.arccos(np.dot(N_vec, e_vec) / (N * ecc))
+
+        # True anomaly
+        ta = np.arccos(np.dot(r_vec / r, e_vec / ecc))
+
+
+        return (sma, ecc, inc, ta, aop, raan)
